33f42fef3062a9515f1bfd3e80139c19c9bcfe37
HunterDietrich <70781742+HunterDietrich@users.noreply.github.com>
2020-09-11 23:16:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+#Variable
+# holds a piece of data
+score = 10
+name = "Zelda"
+
+print()
+print(score)
+print("Score: {}".format(score))
+#print(f"Score: {score}")
+
+#Conditionals
+if score == 10:
+    print("Score is 10!")
+elif score ==5:
+    print("Score is 5")
+else:
+    print("Score is not 10")
